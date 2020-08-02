0db7559b5cf7b5ed5b2de1cc34b1272ce75b223d
MLA Study <mlabootcamp@Marks-MacBook-Air.local>
2020-08-01 19:48:38 -0500

    Adding some queries.

--- SQL Resources/VideoGames-queries.sql
+++ SQL Resources/VideoGames-queries.sql
@@ -7,6 +7,24 @@ select count(*) from platform_catalog
 select count(*) from stock_history
 select count(*) from stock
 
--- Query joining all tables:
+-----------
+-- Queries:
+-----------
+
+-- Video game and the ticker symbol of their platform company.
+select v.game_name, s.ticker 
+from video_game v, stock s, platform_catalog pc, game_platform gp
+where
+	v.id = gp.id_game and
+	pc.id = gp.id_platform and
+	s.company_name = pc.company_name
+
+-- Sales by company.
+select v.game_name, s.ticker 
+from video_game v, stock s, platform_catalog pc, game_platform gp
+where
+	v.id = gp.id_game and
+	pc.id = gp.id_platform and
+	s.company_name = pc.company_name
 
 
